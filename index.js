41185909fa4744771ff507e49a2734fffd401a00
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-03-29 11:15:46 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+// returns undefined if the record has no win objects
+
+superbowlWin = (record) => {
+
+    const result = record.find( record => record.result === "W" );
+    
+  // returns undefined if the record has no win objects
+    return !!result ? result.year : undefined;
+  }
